be5b9cb43f4678b5a114504829cd87a116b20352
fluskybiz-lgtm <flusky.biz@gmail.com>
2026-04-01 16:08:49 +0200

    console log

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,5 @@
 export const load = ({ locals }) => {
+    console.log("Subdomain détecté :", locals.subdomain);
 	// On récupère le sous-domaine que le Hook a extrait
 	return {
 		clientName: locals.subdomain || "Aucun client détecté"
